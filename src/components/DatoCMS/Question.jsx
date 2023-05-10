22b3638c3794f6b8a532210534b22e0061267954
rebe7917 <rebe7917@stud.kea.dk>
2023-05-10 13:21:23 +0200

    mixed

--- src/components/DatoCMS/Question.jsx
+++ src/components/DatoCMS/Question.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import Anchor from "../Anchor";
 
 function Question({ content }) {
-    console.log("hej")
+  console.log(content);
   return (
     <div>
       <h2>{content.title}</h2>
       <p>{content.paragraph}</p>
-      <Anchor>{content.knap_one[0].text}</Anchor>
-      <Anchor>{content.knap_two[0].text}</Anchor>
+      <Anchor href={content.knapOne[0].url}>{content.knapOne[0].text}</Anchor>
+      <Anchor href={content.knapTwo[0].url}>{content.knapTwo[0].text}</Anchor>
     </div>
   );
 }
