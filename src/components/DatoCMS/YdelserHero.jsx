2d03116e181ec066b3cde8ccc207dcffd3ccf6f5
rebe7917 <rebe7917@stud.kea.dk>
2023-05-11 17:07:36 +0200

    p

--- src/components/DatoCMS/YdelserHero.jsx
+++ src/components/DatoCMS/YdelserHero.jsx
@@ -5,7 +5,7 @@ function YdelserHero({ content }) {
     <section style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="w-full bg-cover h-[80vh] bg-no-repeat bg-center  mx-auto  grid lg:items-center p-4 mx-auto max-w-1450">
       <article className="p-4 max-w-[1450px]">
         <h1 className="text-gold-light font-tangier mb-4 text-6xl sm:text-7xl">{content.title}</h1>
-        <p className="text-offWhite max-w-md">{content.paragraph}</p>
+        <p className="text-offWhite max-w-lg text-lg">{content.paragraph}</p>
       </article>
     </section>
   );
