0ce60fccd42dc3d10e6a67f04f7dd3e884c709fa
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-05-15 14:10:41 +0200

    fuck i did it again

--- src/pages/smykker.jsx
+++ src/pages/smykker.jsx
@@ -1,6 +1,7 @@
 import { GraphQLClient } from "graphql-request";
 import { smykkerQuery } from "../modules/smykkerQuery";
 import Galleri from "../components/DatoCMS/smykker/Galleri";
+import Hero from "../components/DatoCMS/smykker/Hero";
 
 
 
@@ -13,18 +14,7 @@ export default function Smykker({ data }) {
   return (
     <>
       <section className="flex items-center justify-center pt-16">
-        <div className="flex flex-col items-center justify-center gap-4">
-          <h1 className="text-gold-light text-7xl font-thin font-sagoe">
-          Smykker
-        </h1>
-        <p className="text-center text-offWhite max-w-2xl">
-          Her på siden kan se hvilke smykker jeg producere og designer, hvis du
-          er interesseret i at købe et eller flere produkter kan du kontakte mig
-          gennem kontaktformularen eller kom forbi Fredensborg Briller hver
-          torsdag i åbningstiderne.{" "}
-        </p>
-        </div>
-        
+        <Hero/>
       </section>
       <div className="text-lg flex gap-8 justify-center py-32 bg-grey-dark">
         <Galleri produkter={produkter} />
