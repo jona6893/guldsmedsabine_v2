6ddfb9a45ceafe1cabe6333c79bc609052c4a0dc
rebe7917 <rebe7917@stud.kea.dk>
2023-05-16 15:22:02 +0200

    problems problems

--- src/components/DatoCMS/UdvalgteSmykker.jsx
+++ src/components/DatoCMS/UdvalgteSmykker.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 
 function UdvalgteSmykker({ content }) {
+  console.log(content.UdvalgteSmykker);
   return (
     <section>
       <div>
         {content.UdvalgteSmykker.map((udvalgtSmykke, index) => (
           <div key={index} className="">
-            <img src={udvalgtSmykke.produktBillede.url} alt="" />
-            <h4>{udvalgtSmykke.produktNavn}</h4>
-            <p>{udvalgtSmykke.pris}</p>
+            <img src={udvalgtSmykke.produkt.produktBillede.url} alt="" />
+            <h4>{udvalgtSmykke.produkt.produktNavn}</h4>
+            <p>{udvalgtSmykke.produkt.pris}</p>
           </div>
         ))}
       </div>
@@ -17,15 +18,3 @@ function UdvalgteSmykker({ content }) {
 }
 
 export default UdvalgteSmykker;
-
-/*    <div className="flex  justify-evenly ">
-          {content.udvalgteYdelser.map((udvalgtYdelse, index) => (
-            <div key={index} className="w-72 h-96 bg-grey-dark  rounded">
-              <img src={udvalgtYdelse.foto.url} alt="" className="w-full h-48 object-cover rounded-t" />
-              <article className="p-4 flex flex-col justify-center h-48 ">
-                <h3 className="text-gold-light">{udvalgtYdelse.overskrift}</h3>
-                <p className="text-offWhite ">{udvalgtYdelse.broedtekst}</p>
-              </article>
-            </div>
-          ))}
-        </div> */
