6ab5f725abda3c7357d0f88018a0b8785d52b9bc
rebe7917 <rebe7917@stud.kea.dk>
2023-05-17 09:20:24 +0200

    grid?

--- src/components/DatoCMS/UdvalgteSmykker.jsx
+++ src/components/DatoCMS/UdvalgteSmykker.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function UdvalgteSmykker({ content }) {
   console.log(content.UdvalgteSmykker);
   return (
-    <section>
+    <section className="py-16 max-w-[1450px] mx-auto grid grid-cols-4">
       <div>
         {content.udvalgteSmykker.map((udvalgtSmykke, index) => (
           <div key={index} className="">
-            <img src={udvalgtSmykke.produktBillede[0].url} alt="" />
+            <img src={udvalgtSmykke.produktBillede[0].url} alt="" className="w-80" />
             <h4>{udvalgtSmykke.produktNavn}</h4>
             <p>{udvalgtSmykke.pris}</p>
           </div>
