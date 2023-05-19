444a8419027abfc1d039fba13b22b6faa97d21fa
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 10:46:09 +0200

    kort done

--- src/components/DatoCMS/forside/FindMigHer.jsx
+++ src/components/DatoCMS/forside/FindMigHer.jsx
@@ -3,11 +3,11 @@ import Anchor from "../../Anchor";
 function FindMigHer({ content }) {
   console.log(content.kort);
   return (
-    <section className="max-[890px]:pb-16 min-[890px]:py-16 min-[890px]:px-4  flex max-sm:flex-col justify-evenly items-center mx-auto max-w-[1450px] gap-8">
-      <div className="w-full sm:w-96 h-80 ">
+    <section className=" grid md:grid-cols-2 items-center mx-auto max-w-[1450px] justify-items-center pb-8">
+      <div className="max-md:h-[300px] h-[500px] w-full ">
         <iframe width="100%" height="100%" src={"https://maps.google.com/maps?q=" + content.kort.latitude + "," + content.kort.longitude + "&t=&z=19&ie=UTF8&iwloc=&output=embed"}></iframe>
       </div>
-      <article className="max-sm:order-first max-[890px]:px-4 max-[890px]:pt-8">
+      <article className="max-md:order-first px-4 py-16 md:px-6">
         <h2 className="text-gold-light text-4xl font-light mb-4">{content.overskrift}</h2>
         <p className="text-offWhite max-w-md mb-6">{content.broedtekst}</p>
         <Anchor className="text-gold-light border-gold-dark border-2 rounded-full w-[180px] flex items-center justify-center py-1.5" href={content.knap[0].url}>
