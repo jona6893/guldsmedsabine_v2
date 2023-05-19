e884b206b953c7c0c34b46596e7bd4d66a219333
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 14:58:36 +0200

    fredenborg briller

--- src/components/DatoCMS/omSabine/Informaioner.jsx
+++ src/components/DatoCMS/omSabine/Informaioner.jsx
@@ -1,25 +1,24 @@
+import Anchor from "../../Anchor";
+
+
 function Informaioner({ content }) {
   return (
-    <section className="bg-grey-dark text-offWhite py-16 ">
-      <div className="px-5 grid md:grid-cols-2 max-md:gap-16 max-w-[1450px] mx-auto gap-4 justify-items-end">
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-2">
-            <h2 className="text-gold-light text-5xl font-extralight">{content[3].heading}</h2>
-            <h3 className="text-gold-light text-4xl font-tangier">{content[3].subHeading}</h3>
-          </div>
-          <p>{content[3].paragraph}</p>
-          <a
-            className="w-fit bg-gold-dark border-gold-dark border hover:bg-transparent hover:text-gold-light hover:border-gold-light py-2 p-8 text-offWhite rounded-full"
-            href={content[3].knap[0].url}
-          >
+    <section className="bg-grey-dark text-offWhite px-5 py-16">
+      <div className=" grid lg:flex lg:justify-evenly  max-w-[1450px] mx-auto gap-8 justify-items-center content-center items-center ">
+        <article className=" grid gap-6">
+          <h2 className="text-gold-light text-5xl font-extralight">{content[3].heading}</h2>
+          <h3 className="text-gold-light text-4xl font-tangier ">{content[3].subHeading}</h3>
+
+          <p className="max-w-xl mb-4">{content[3].paragraph}</p>
+
+          <Anchor className="bg-gold-dark text-offWhite rounded-full px-4 w-max flex items-center justify-center py-2" href={content[3].knap[0].url}>
             {content[3].knap[0].text}
-          </a>
-        </div>
-        <div className="flex flex-col gap-8">
+          </Anchor>
+        </article>
+
+        <div className="max-lg:w-full max-lg:max-w-xl grid gap-8">
           <div>
-            <h4 className="text-gold-light text-xl">
-              {content[3].aabningstiderHeading}
-            </h4>
+            <h4 className="text-gold-light text-xl">{content[3].aabningstiderHeading}</h4>
             <p>{content[3].tider}</p>
           </div>
           <div>
