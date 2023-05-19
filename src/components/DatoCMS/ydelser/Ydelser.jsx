140aca15118b18dc12dbd3217fdc0a86d20a2773
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 13:48:57 +0200

    ydelser

--- src/components/DatoCMS/ydelser/Ydelser.jsx
+++ src/components/DatoCMS/ydelser/Ydelser.jsx
@@ -6,14 +6,14 @@ function Ydelser({ content }) {
           <section className=" bg-grey-dark grid justify-items-center">
             <div className=" grid sm:grid-cols-2 max-w-[1450px] mx-auto">
               <div className="flex flex-col  justify-center items-center">
-                <article className="max-w-lg p-4 ">
+                <article className="max-w-lg px-4 py-16 sm:p-4 ">
                   <h2 className="text-gold-light text-4xl mb-4 font-light uppercase">{ydelse.title}</h2>
                   <p className="text-offWhite mb-4">{ydelse.paragraph}</p>
                   <p className="text-gold-light text-2xl font-light">{ydelse.price}</p>
                 </article>
               </div>
 
-              <img className={`w-[726px] h-[500px] object-cover  ${index % 2 == 0 ? "order-first" : ""}`} src={ydelse.photo.url} alt="" />
+              <img className={`w-[726px] h-[500px] object-cover  ${index % 2 == 0 ? "sm:order-first" : ""}`} src={ydelse.photo.url} alt="" />
             </div>
           </section>
         );
