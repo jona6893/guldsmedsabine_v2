94a7b412b220c1fdfb34500f1ad58c544bfaa37b
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 15:06:34 +0200

    mx

--- src/components/DatoCMS/omSabine/OmMig.jsx
+++ src/components/DatoCMS/omSabine/OmMig.jsx
@@ -8,7 +8,7 @@ function OmMig({ content }) {
           <div className=" px-5 py-16 grid justify-items-center">
             <article>
               <h2 className="font-thin text-5xl text-gold-light mb-4">{content[1].title}</h2>
-              <p className="max-w-xl">{content[1].paragraph}</p>
+              <p className="max-w-lg text-base">{content[1].paragraph}</p>
             </article>
           </div>
 
@@ -17,10 +17,10 @@ function OmMig({ content }) {
           <div className="px-5 max-md:py-16 md:pb-16 grid gap-8 justify-items-center">
             <article>
               <h3 className="font-thin text-4xl text-gold-light mb-4">{content[1].title2}</h3>
-              <p className="max-w-xl">{content[1].paragraph2}</p>
+              <p className="max-w-lg text-base">{content[1].paragraph2}</p>
             </article>
 
-            <div className="w-full max-w-xl">
+            <div className="w-full max-w-lg">
               <Anchor className="bg-gold-dark text-offWhite rounded-full px-4 flex items-center justify-center w-max py-2" href={content[1].knap[0].url}>
                 {content[1].knap[0].text}
               </Anchor>
