9371101fa1fd6296c0eea7d60f654fae1bd00fe8
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 12:04:24 +0200

    om sabine hero

--- src/components/DatoCMS/omSabine/Hero.jsx
+++ src/components/DatoCMS/omSabine/Hero.jsx
@@ -4,11 +4,11 @@ function Hero({ content }) {
   return (
     <section
       style={{ backgroundImage: `url("${content[0].heroImage.url}")` }}
-      className="h-screen sm:bg-cover max-sm:bg-right bg-center">
-      <div className="grid md:grid-cols-2 content-center p-4 h-full bg-black/50 max-w-[1450px] mx-auto">
-        <div className="sm:p-8 inset-0">
+      className="h-[70vh] bg-cover  bg-center">
+      <div className="grid  h-full  items-center  max-w-[1450px] mx-auto">
+        <div className="px-4 ">
           <h1 className="text-gold-light font-tangier">{content[0].title}</h1>
-          <p className="text-offWhite">{content[0].paragraph}</p>
+          <p className="text-offWhite max-w-md">{content[0].paragraph}</p>
         </div>
       </div>
     </section>
