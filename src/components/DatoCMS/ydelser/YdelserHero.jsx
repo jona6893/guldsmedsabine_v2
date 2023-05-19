4d11a99fb65ba9de705015260372af9798a70e18
rebe7917 <rebe7917@stud.kea.dk>
2023-05-19 12:48:35 +0200

    ydelser

--- src/components/DatoCMS/ydelser/YdelserHero.jsx
+++ src/components/DatoCMS/ydelser/YdelserHero.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function YdelserHero({ content }) {
   return (
-    <section style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="w-full bg-cover h-[80vh] bg-no-repeat bg-center    p-4 mx-auto max-w-1450">
-      <div className="">
-        <article className="p-4 max-w-[1450px] mx-auto">
+    <section style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="w-full bg-cover h-[70vh] bg-no-repeat bg-center    p-4 mx-auto max-w-1450">
+      <div className="grid  h-full  sm:items-center  max-w-[1450px] mx-auto">
+        <article className="px-4">
           <h1 className="text-gold-light font-tangier mb-4 text-6xl sm:text-7xl">{content.title}</h1>
           <p className="text-offWhite max-w-lg text-lg">{content.paragraph}</p>
         </article>
