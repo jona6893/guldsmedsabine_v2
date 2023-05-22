2c59d08d9499d50e338cd31208c665b77f323070
rebe7917 <rebe7917@stud.kea.dk>
2023-05-22 14:57:27 +0200

    vandmærke

--- src/components/DatoCMS/kontakt/Kontakt.jsx
+++ src/components/DatoCMS/kontakt/Kontakt.jsx
@@ -5,8 +5,8 @@ export default function Kontakt({ content }) {
   const { overskrift, paragraf, billede, formular } = content;
 
   return (
-    <section className="grid lg:grid-cols-2 max-w-[1450px] mx-auto">
-      <div className="relative px-6 sm:px-12 lg:px-16 xl:px-24  py-16 flex flex-col gap-8 max-w-2xl mx-auto">
+    <section className="grid lg:grid-cols-2 max-w-[1450px] mx-auto  ">
+      <div className="relative px-6 sm:px-12 lg:px-16 xl:px-24  py-16 flex flex-col gap-8 max-w-2xl mx-auto bg-no-repeat bg-cover bg-center" style={{ backgroundImage: `url("/images/sabineLogoBaground.svg")` }}>
         <button className="absolute top-0 lg:top-4 left-6 sm:left-12 lg:left-16 xl:left-24 text-offWhite text-4xl" onClick={() => Router.back()}>
           ←
         </button>
