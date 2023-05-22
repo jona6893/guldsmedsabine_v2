3b1b9926a8c94c95444fd6103428f78d60cd8782
rebe7917 <rebe7917@stud.kea.dk>
2023-05-22 12:45:57 +0200

    kort

--- src/components/DatoCMS/forside/FindMigHer.jsx
+++ src/components/DatoCMS/forside/FindMigHer.jsx
@@ -4,8 +4,8 @@ function FindMigHer({ content }) {
   console.log(content.kort);
   return (
     <section className=" grid md:grid-cols-2 items-center mx-auto max-w-[1450px] justify-items-center pb-8">
-      <div className="max-md:h-[300px] h-[500px] w-full ">
-        <iframe width="100%" height="100%" src={"https://maps.google.com/maps?q=" + content.kort.latitude + "," + content.kort.longitude + "&t=&z=19&ie=UTF8&iwloc=&output=embed"}></iframe>
+      <div className="max-md:h-[300px] h-[500px] w-full  grayscale">
+        <iframe  width="100%" height="100%" src={"https://maps.google.com/maps?q=" + content.kort.latitude + "," + content.kort.longitude + "&t=&z=19&ie=UTF8&iwloc=&output=embed"}></iframe>
       </div>
       <article className="max-md:order-first px-4 py-16 md:px-6">
         <h2 className="text-gold-light text-4xl font-light mb-4">{content.overskrift}</h2>
