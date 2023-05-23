0215b748f50aee3f95a7877d9d8565d47f8692d6
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-05-23 11:29:33 +0200

    statement i layout

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,15 +1,21 @@
 import Footer from "../components/Footer";
 import GotoTop from "../components/GotoTop";
 import Navigation from "../components/Navigation";
+import { useRouter } from "next/router";
 
 // Frontend
 export default function Layout({ children, navData }) {
+ const router = useRouter();
+ const currentUrl = router.asPath;
+ console.log(currentUrl);
   return (
     <>
-      <Navigation navData={navData} />
+    {currentUrl !== '/kontakt-feedback' &&  <Navigation navData={navData} />}
+     
       {children}
       <GotoTop/>
-      <Footer/>
+      {currentUrl !== '/kontakt-feedback' && <Footer/>}
+      
     </>
   );
 }
