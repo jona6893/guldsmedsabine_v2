a0ce1275e4920ce114536015bce1d3523e9dad55
Victor Ly <ly.victor2000@gmail.com>
2023-05-23 12:02:19 +0200

    fix

--- src/pages/ydelser.jsx
+++ src/pages/ydelser.jsx
@@ -18,13 +18,15 @@ export default function YdelserPage({ data }) {
             case "YdelserHeroRecord":
               return <YdelserHero content={content} />;
 
-          case "YdelserRecord":
-            return <Ydelser key={content.id} content={content} />;
-          case "SpoergsmaalRecord":
-            return <Question key={content.id} content={content} />;
-        }
-      })}
-    </main>
+            case "YdelserRecord":
+              return <Ydelser key={content.id} content={content} />;
+            case "SpoergsmaalRecord":
+              return <Question key={content.id} content={content} />;
+          }
+        })}
+      </main>
+      <Footer content={footer[0].content[0]} />
+    </>
   );
 }
 
