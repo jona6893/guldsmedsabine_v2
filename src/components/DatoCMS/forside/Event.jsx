8e0a3942711ffa45606dc1e606754588a143732c
Victor Ly <ly.victor2000@gmail.com>
2023-05-23 14:24:10 +0200

    minor changes

--- src/components/DatoCMS/forside/Event.jsx
+++ src/components/DatoCMS/forside/Event.jsx
@@ -3,52 +3,31 @@ import Anchor from "../../Anchor";
 
 function Event({ content }) {
   return (
-    <section
-      id="events"
-      className="mx-auto max-w-[1450px] grid md:grid-cols-2 justify-items-center items-center    "
-    >
+    <section id="events" className="mx-auto max-w-[1450px] grid md:grid-cols-2 justify-items-center items-center    ">
       <div className=" flex flex-col gap-10  px-4 py-8">
         <article className="">
-          <h2 className="text-gold-light text-4xl font-light mb-4">
-            {content.title}
-          </h2>
+          <h2 className="text-gold-light text-4xl font-light mb-4">{content.title}</h2>
 
           <p className="text-offWhite max-w-lg mb-4">{content.paragraph}</p>
-          <h4 className="text-offWhite font-tangier text-4xl">
-            {content.smallTitle}
-          </h4>
+          <h4 className="text-offWhite font-tangier text-4xl">{content.smallTitle}</h4>
         </article>
         <article className="grid gap-2">
-          <h3 className="text-gold-light text-2xl font-light">
-            {" "}
-            {content.titleTwo}
-          </h3>
+          <h3 className="text-gold-light text-2xl font-light"> {content.titleTwo}</h3>
           <p className="flex gap-2 text-offWhite">
-            {content.titleDate}{" "}
-            <span className="text-gold-light">{content.date}</span>
+            {content.titleDate} <span className="text-gold-light">{content.date}</span>
           </p>
           <p className="flex gap-4 text-offWhite">
-            {content.titleTid}{" "}
-            <span className="text-gold-light">{content.time}</span>
+            {content.titleTid} <span className="text-gold-light">{content.time}</span>
           </p>
           <p className="flex gap-2 text-offWhite">
-            {content.titleLocation}{" "}
-            <span className="text-gold-light">{content.location}</span>
+            {content.titleLocation} <span className="text-gold-light">{content.location}</span>
           </p>
         </article>
-        <div className="flex max-sm:flex-col md:flex-col min-[915px]:flex-row gap-y-8 justify-between">
-          <Anchor
-            className="button-primary"
-            target="_blank"
-            href={content.knapper[0]?.url}
-          >
+        <div className="flex flex-row gap-x-4 gap-y-8 justify-between flex-wrap">
+          <Anchor className="button-primary" target="_blank" href={content.knapper[0]?.url}>
             {content.knapper[0]?.text}
           </Anchor>
-          <Anchor
-            className="button-secondary"
-            target="_blank"
-            href={content.knapper[1]?.url}
-          >
+          <Anchor className="button-secondary" target="_blank" href={content.knapper[1]?.url}>
             {content.knapper[1]?.text}
           </Anchor>
         </div>
