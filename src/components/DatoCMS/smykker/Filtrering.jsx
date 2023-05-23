64e7c0e4b1e6b7cdb74d55cdd6f2ecf5a84ca172
rebe7917 <rebe7917@stud.kea.dk>
2023-05-23 14:54:15 +0200

    vandmærke

--- src/components/DatoCMS/smykker/Filtrering.jsx
+++ src/components/DatoCMS/smykker/Filtrering.jsx
@@ -39,7 +39,7 @@ export default function Filtrering({ kategori, materiale, setKategoriFilter, set
           <svg className="absolute w-8 h-8 bottom-1 right-4 pointer-events-none text-gold-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
             <path d="M480 711 240 471l43-43 197 198 197-197 43 43-240 239Z" />
           </svg>
-          <select className="appearance-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-transparent border-2 border-gold-dark text-gold-light hover:bg-grey-dark hover:outline-none " onChange={() => handleMaterialeChange(event)}>
+          <select className="appearance-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-transparent border-2 border-gold-dark text-gold-light hover:bg-grey-dark hover:outline-none outline-gold-light " onChange={() => handleMaterialeChange(event)}>
             <option value="Alle" className=" ">
               Alle
             </option>
