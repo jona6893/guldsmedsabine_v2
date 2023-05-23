d66acd757ccfddf76b338bf9db8baa6736e870ca
rebe7917 <rebe7917@stud.kea.dk>
2023-05-23 14:38:30 +0200

    style

--- src/components/DatoCMS/smykker/Filtrering.jsx
+++ src/components/DatoCMS/smykker/Filtrering.jsx
@@ -16,20 +16,12 @@ export default function Filtrering({ kategori, materiale, setKategoriFilter, set
   return (
     <div className="flex gap-4 items-start w-full justify-center ">
       <div className="flex flex-row gap-8 flex-wrap items-center">
-        <label className="relative">
-          <span className="block text-offWhite text-center">Kategori:</span>
-          <svg
-            className="absolute w-8 h-8 bottom-1 right-4 pointer-events-none text-offWhite"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 96 960 960"
-          >
+        <label className="relative md:flex items-center gap-4">
+          <span className="block text-offWhite text-center mb-2">Kategori:</span>
+          <svg className="absolute w-8 h-8 bottom-1 right-4 pointer-events-none text-gold-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
             <path d="M480 711 240 471l43-43 197 198 197-197 43 43-240 239Z" />
           </svg>
-          <select
-            className="appearance-none border-none outline-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-gold-light text-offWhite"
-            onChange={() => handleKategoriChange(event)}
-          >
+          <select className="appearance-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-transparent border-2 border-gold-dark text-gold-light hover:bg-grey-dark" onChange={() => handleKategoriChange(event)}>
             <option value="Alle" className="">
               Alle
             </option>
@@ -42,21 +34,13 @@ export default function Filtrering({ kategori, materiale, setKategoriFilter, set
             })}
           </select>
         </label>
-        <label className="relative">
-          <span className="block text-offWhite text-center">Materiale:</span>
-          <svg
-            className="absolute w-8 h-8 bottom-1 right-4 pointer-events-none text-offWhite"
-            fill="currentColor"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 96 960 960"
-          >
+        <label className="relative md:flex items-center gap-4">
+          <span className="block text-offWhite text-center mb-2">Materiale:</span>
+          <svg className="absolute w-8 h-8 bottom-1 right-4 pointer-events-none text-gold-light" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 96 960 960">
             <path d="M480 711 240 471l43-43 197 198 197-197 43 43-240 239Z" />
           </svg>
-          <select
-            className="appearance-none border-none outline-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-gold-light text-offWhite"
-            onChange={() => handleMaterialeChange(event)}
-          >
-            <option value="Alle" className="">
+          <select className="appearance-none rounded-full py-2 pl-8 pr-12 cursor-pointer bg-transparent border-2 border-gold-dark text-gold-light hover:bg-grey-dark hover:outline-none " onChange={() => handleMaterialeChange(event)}>
+            <option value="Alle" className=" ">
               Alle
             </option>
             {materiale.map((materiale) => {
