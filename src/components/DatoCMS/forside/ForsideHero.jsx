980d3d8103c2c791316928d2ca45d2cd029b5239
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-05-30 09:30:14 +0200

    Forside hero tekst

--- src/components/DatoCMS/forside/ForsideHero.jsx
+++ src/components/DatoCMS/forside/ForsideHero.jsx
@@ -4,17 +4,29 @@ export default function ForsideHero({ content }) {
   console.log(content)
   return (
     <section className="max-w-[1450px] mx-auto">
-      <video muted autoPlay loop className="w-full max-md:hidden">
-        <source src={content.baggrundsvideo?.url} type="video/mp4" />
-      </video>
-         <Image
-        className="col-start-1 row-span-1 object-cover h-full aspect-square md:hidden"
-        width={content.mobileBaggrund?.width}
-        height={content.mobileBaggrund?.height}
-        src={content.mobileBaggrund?.url}
-        alt={content.mobileBaggrund?.alt}
-        priority={true}
-      />
+      <div className="grid max-md:hidden">
+        <div className="bg-black/50 col-start-1 row-start-1 text-offWhite z-[1] flex flex-col justify-center p-4 gap-4">
+          <h1 className="">Sabine guldsmid</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <video muted autoPlay loop className="w-full col-start-1 row-start-1 ">
+          <source src={content.baggrundsvideo?.url} type="video/mp4" />
+        </video>
+      </div>
+      <div className="grid md:hidden">
+        <div className="bg-black/50 col-start-1 row-start-1 text-offWhite z-[1] flex flex-col justify-center p-4 gap-4">
+          <h1 className="">Sabine guldsmid</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <Image
+          className="col-start-1 row-span-1 object-cover h-full col-start-1 row-start-1 aspect-square "
+          width={content.mobileBaggrund?.width}
+          height={content.mobileBaggrund?.height}
+          src={content.mobileBaggrund?.url}
+          alt={content.mobileBaggrund?.alt}
+          priority={true}
+        />
+      </div>
     </section>
   );
 }
