beb8a57811cdadc6715f68027e16c9715ec4d723
Victor Ly <ly.victor2000@gmail.com>
2023-05-30 12:39:05 +0200

    brightness filter on background image

--- src/components/DatoCMS/ydelser/YdelserHero.jsx
+++ src/components/DatoCMS/ydelser/YdelserHero.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function YdelserHero({ content }) {
   return (
-    <section style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="w-full bg-cover h-[70vh] bg-no-repeat bg-center ">
-      <div className="h-full max-w-[1450px] mx-auto flex items-center px-4">
+    <section className="relative w-full h-[70vh]">
+      <div
+        style={{ backgroundImage: `url("${content.heroImage.url}")` }}
+        className="absolute h-full w-full bg-cover bg-no-repeat bg-center brightness-50 md:brightness-100"
+      />
+      <div className="relative h-full max-w-[1450px] mx-auto flex items-center px-4">
         <article className="p-4 ">
           <h1 className="text-gold-light font-italianno mb-4 ">{content.title}</h1>
           <p className="text-offWhite max-w-xl text-lg">{content.paragraph}</p>
