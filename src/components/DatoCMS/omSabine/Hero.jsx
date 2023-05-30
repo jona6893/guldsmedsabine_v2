88472043f2241122b481345b0f9c38f40d2e9f42
Victor Ly <ly.victor2000@gmail.com>
2023-05-30 12:31:35 +0200

    brightness filter on background image

--- src/components/DatoCMS/omSabine/Hero.jsx
+++ src/components/DatoCMS/omSabine/Hero.jsx
@@ -1,10 +1,11 @@
 function Hero({ content }) {
   return (
-    <section style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="h-[70vh] bg-cover  bg-center">
-      <div className="grid  h-full  items-center  max-w-[1450px] mx-auto">
+    <section className="relative h-[70vh]">
+      <div style={{ backgroundImage: `url("${content.heroImage.url}")` }} className="absolute h-full w-full bg-cover bg-center brightness-50" />
+      <div className="relative grid h-full items-center max-w-[1450px] mx-auto">
         <div className="px-4 ">
           <h1 className="text-gold-light font-italianno">{content.title}</h1>
-          <p className="text-offWhite max-w-xl ">{content.paragraph}</p>
+          <p className="text-offWhite max-w-xl">{content.paragraph}</p>
         </div>
       </div>
     </section>
