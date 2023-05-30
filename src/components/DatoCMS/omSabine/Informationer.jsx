f4ca65e778fed5c602539b64949a3400e8c37e37
rebe7917 <rebe7917@stud.kea.dk>
2023-05-30 10:35:31 +0200

    uppercase

--- src/components/DatoCMS/omSabine/Informationer.jsx
+++ src/components/DatoCMS/omSabine/Informationer.jsx
@@ -5,7 +5,7 @@ function Informationer({ content }) {
     <section className="bg-grey-dark text-offWhite px-4 py-16 bg-no-repeat bg-contain" style={{ backgroundImage: `url("/images/gren_seks.svg")`, backgroundPosition: "100% 90%", backgroundSize: "" }}>
       <div className=" grid lg:flex lg:justify-evenly  max-w-[1450px] mx-auto gap-8 justify-items-center content-center items-center ">
         <article className=" grid gap-6">
-          <h2 className="text-gold-light font-extralight">{content.heading}</h2>
+          <h2 className="text-gold-light font-extralight uppercase">{content.heading}</h2>
           <h3 className="text-gold-light font-italianno text-4xl">{content.subHeading}</h3>
 
           <p className="max-w-xl mb-4">{content.paragraph}</p>
@@ -17,11 +17,11 @@ function Informationer({ content }) {
 
         <div className="max-lg:w-full max-lg:max-w-xl grid gap-8">
           <div>
-            <h4 className="text-gold-light ">{content.aabningstiderHeading}</h4>
+            <h4 className="text-gold-light uppercase">{content.aabningstiderHeading}</h4>
             <p>{content.tider}</p>
           </div>
           <div>
-            <h4 className="text-gold-light ">{content.lokationHeading}</h4>
+            <h4 className="text-gold-light uppercase">{content.lokationHeading}</h4>
             <p>{content.lokation}</p>
           </div>
         </div>
