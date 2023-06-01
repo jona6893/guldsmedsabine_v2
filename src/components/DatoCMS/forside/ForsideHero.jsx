acd93a5758bdc26b1d416ba9c8f8db9eaad8b72b
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-01 11:40:00 +0200

    preload off

--- src/components/DatoCMS/forside/ForsideHero.jsx
+++ src/components/DatoCMS/forside/ForsideHero.jsx
@@ -36,23 +36,37 @@ export default function ForsideHero({ content }) {
       <div className="grid max-md:hidden relative h-[80vh]">
         <div className="bg-black/25 col-start-1 row-start-1 z-[1]">
           <div className=" text-offWhite flex flex-col justify-center p-4 gap-4 max-w-[1450px] mx-auto w-full h-full">
-            <h1 className="text-gold-light font-italianno mb-2 text-9xl">{content.overskrift}</h1>
-            <h2 className="text-gold-light uppercase font-light mb-2 ">{content.subOverskrift}</h2>
+            <h1 className="text-gold-light font-italianno mb-2 text-9xl">
+              {content.overskrift}
+            </h1>
+            <h2 className="text-gold-light uppercase font-light mb-2 ">
+              {content.subOverskrift}
+            </h2>
             <p className="mb-8 text-offWhite max-w-lg">{content.brDtekst}</p>
             <Anchor className="button-primary" href={content.knap[0].url}>
               {content.knap[0].text}
             </Anchor>
           </div>
         </div>
-        <video ref={videoRef} muted autoPlay loop className="absolute top-0 left-0 w-full h-full object-cover z-0">
+        <video
+          ref={videoRef}
+          muted
+          preload="none"
+          loop
+          className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        >
           <source src={content.baggrundsvideo?.url} type="video/mp4" />
         </video>
       </div>
       {/* mobile */}
       <div className="grid md:hidden h-[70vh]">
         <div className="bg-black/50 col-start-1 row-start-1 text-offWhite z-[1] flex flex-col justify-center p-4 gap-4">
-          <h1 className="text-gold-light font-italianno mb-4">{content.overskrift}</h1>
-          <h2 className="text-gold-light uppercase font-light mb-4 text-2xl">{content.subOverskrift}</h2>
+          <h1 className="text-gold-light font-italianno mb-4">
+            {content.overskrift}
+          </h1>
+          <h2 className="text-gold-light uppercase font-light mb-4 text-2xl">
+            {content.subOverskrift}
+          </h2>
           <p className="mb-8 text-offWhite max-w-lg">{content.brDtekst}</p>
           <Anchor className="button-primary" href={content.knap[0].url}>
             {content.knap[0].text}
