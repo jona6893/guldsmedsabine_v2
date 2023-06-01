ad4f1e0e21c9ff4f91b3e5ef332455773c2e9a9a
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-06-01 11:22:02 +0200

    🤞

--- src/components/DatoCMS/forside/ForsideHero.jsx
+++ src/components/DatoCMS/forside/ForsideHero.jsx
@@ -44,42 +44,60 @@ export default function ForsideHero({ content }) {
   //console.log(content);
   return (
     <section className="mx-auto">
-{!isMobile ? 
-      <div className="grid relative h-[80vh]">
-        <div className="bg-black/25 col-start-1 row-start-1 z-[1]">
-          <div className=" text-offWhite flex flex-col justify-center p-4 gap-4 max-w-[1450px] mx-auto w-full h-full">
-            <h1 className="text-gold-light font-italianno mb-2 text-9xl">{content.overskrift}</h1>
-            <h2 className="text-gold-light uppercase font-light mb-2 ">{content.subOverskrift}</h2>
+      {!isMobile ? (
+        <div className="grid relative h-[80vh]">
+          <div className="bg-black/25 col-start-1 row-start-1 z-[1]">
+            <div className=" text-offWhite flex flex-col justify-center p-4 gap-4 max-w-[1450px] mx-auto w-full h-full">
+              <h1 className="text-gold-light font-italianno mb-2 text-9xl">
+                {content.overskrift}
+              </h1>
+              <h2 className="text-gold-light uppercase font-light mb-2 ">
+                {content.subOverskrift}
+              </h2>
+              <p className="mb-8 text-offWhite max-w-lg">{content.brDtekst}</p>
+              <Anchor className="button-primary" href={content.knap[0].url}>
+                {content.knap[0].text}
+              </Anchor>
+            </div>
+          </div>
+          <video
+            ref={videoRef}
+            muted
+            autoPlay
+            loop
+            className="absolute top-0 left-0 w-full h-full object-cover z-0"
+          >
+            <source
+              src={content.baggrundsvideo?.url}
+              type="video/mp4"
+              media="(min-width: 640px)"
+            />
+          </video>
+        </div>
+      ) : (
+        <div className="grid  h-[70vh]">
+          <div className="bg-black/50 col-start-1 row-start-1 text-offWhite z-[1] flex flex-col justify-center p-4 gap-4">
+            <h1 className="text-gold-light font-italianno mb-4">
+              {content.overskrift}
+            </h1>
+            <h2 className="text-gold-light uppercase font-light mb-4 text-2xl">
+              {content.subOverskrift}
+            </h2>
             <p className="mb-8 text-offWhite max-w-lg">{content.brDtekst}</p>
             <Anchor className="button-primary" href={content.knap[0].url}>
               {content.knap[0].text}
             </Anchor>
           </div>
+          <Image
+            className="col-start-1 row-span-1 object-cover h-full col-start-1 row-start-1 aspect-square "
+            width={content.mobileBaggrund?.width}
+            height={content.mobileBaggrund?.height}
+            src={content.mobileBaggrund?.url}
+            alt={content.mobileBaggrund?.alt}
+            priority={true}
+          />
         </div>
-        <video ref={videoRef} muted autoPlay loop className="absolute top-0 left-0 w-full h-full object-cover z-0">
-          <source src={!isMobile && content.baggrundsvideo?.url} type="video/mp4" />
-        </video>
-      </div>
-      :
-      <div className="grid  h-[70vh]">
-        <div className="bg-black/50 col-start-1 row-start-1 text-offWhite z-[1] flex flex-col justify-center p-4 gap-4">
-          <h1 className="text-gold-light font-italianno mb-4">{content.overskrift}</h1>
-          <h2 className="text-gold-light uppercase font-light mb-4 text-2xl">{content.subOverskrift}</h2>
-          <p className="mb-8 text-offWhite max-w-lg">{content.brDtekst}</p>
-          <Anchor className="button-primary" href={content.knap[0].url}>
-            {content.knap[0].text}
-          </Anchor>
-        </div>
-        <Image
-          className="col-start-1 row-span-1 object-cover h-full col-start-1 row-start-1 aspect-square "
-          width={content.mobileBaggrund?.width}
-          height={content.mobileBaggrund?.height}
-          src={content.mobileBaggrund?.url}
-          alt={content.mobileBaggrund?.alt}
-          priority={true}
-        />
-      </div>
-      }
+      )}
     </section>
   );
 }
